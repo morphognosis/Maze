ab2432b260e8cfcd97eff81d020029162683a231
Tom Portegys <portegys@gmail.com>
2021-01-22 22:00:23 -0600

    build shell script

--- work/build.sh
+++ work/build.sh
@@ -2,4 +2,4 @@
 javac -classpath "../lib/morphognosis.jar:../lib/weka.jar" -d . ../src/morphognosis/*.java ../src/morphognosis/maze/*.java
 cp ../src/morphognosis/maze/maze_maker.py .
 cp ../src/morphognosis/maze/maze_rnn.py .
-jar cvfm ../bin/maze.jar honey_bees.mf morphognosis maze_maker.py maze_rnn.py
+jar cvfm ../bin/maze.jar maze.mf morphognosis maze_maker.py maze_rnn.py
