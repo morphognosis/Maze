c5a66439dd6228be0c9e342e79191ba250baff03
Tom Portegys <portegys@gmail.com>
2021-03-12 21:58:02 -0600

    Update build scripts

--- work/build.bat
+++ work/build.bat
@@ -1,4 +1,4 @@
-javac -classpath "../lib/morphognosis.jar;../lib/weka.jar" -d . ../src/morphognosis/*.java ../src/morphognosis/maze/*.java
+javac -classpath "../lib/weka.jar" -d . ../src/morphognosis/*.java ../src/morphognosis/maze/*.java
 copy ..\src\morphognosis\maze\maze_maker.py .
 copy ..\src\morphognosis\maze\maze_rnn.py .
 jar cvfm ../bin/maze.jar maze.mf morphognosis maze_maker.py maze_rnn.py
--- work/build.sh
+++ work/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-javac -classpath "../lib/morphognosis.jar:../lib/weka.jar" -d . ../src/morphognosis/*.java ../src/morphognosis/maze/*.java
+javac -classpath "../lib/weka.jar" -d . ../src/morphognosis/*.java ../src/morphognosis/maze/*.java
 cp ../src/morphognosis/maze/maze_maker.py .
 cp ../src/morphognosis/maze/maze_rnn.py .
 jar cvfm ../bin/maze.jar maze.mf morphognosis maze_maker.py maze_rnn.py
